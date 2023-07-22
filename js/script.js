8d3800ffe987078fe4c61bed951fd2dfa4bd82d1
Radek <rj.airoo@gmail.com>
2023-07-22 23:51:23 +0200

    added js file and querySelectors

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const deleteBtn = document.querySelector('.delete')
+const addBtn = document.querySelector('.add-transaction')
+const deleteAllBtn = document.querySelector('.delete-all')
+const lightBtn = document.querySelector('.light')
+const darkBtn = document.querySelector('.dark')
+
+const transactionName = document.querySelector('.transaction-name')
+const transactionAmount = document.querySelector('.transaction-amount')
+const availableMoney = document.querySelector('.available-money')
+
+const inputName = document.querySelector('#name')
+const inputAmount = document.querySelector('#amount')
+const selectCategory = document.querySelector('#category')
+
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
