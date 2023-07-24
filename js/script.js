f63905cad140e39a04fff8472f72a45a374d4a04
Radek <rj.airoo@gmail.com>
2023-07-24 22:58:47 +0200

    added show and closePanel fuctions

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
-const deleteBtn = document.querySelector('.delete')
+const incomeArea = document.querySelector('.income-area')
+const expensesArea = document.querySelector('.expenses-area')
+const availableMoney = document.querySelector('.available-money')
+
+const addTransactionPanel = document.querySelector('.add-transaction-panel')
+const nameInput = document.querySelector('#name')
+const amountInput = document.querySelector('#amount')
+const categorySelect = document.querySelector('#category')
+
 const addBtn = document.querySelector('.add-transaction')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deleteBtn = document.querySelector('.delete')
 const deleteAllBtn = document.querySelector('.delete-all')
 const lightBtn = document.querySelector('.light')
 const darkBtn = document.querySelector('.dark')
 
 const transactionName = document.querySelector('.transaction-name')
 const transactionAmount = document.querySelector('.transaction-amount')
-const availableMoney = document.querySelector('.available-money')
 
-const inputName = document.querySelector('#name')
-const inputAmount = document.querySelector('#amount')
-const selectCategory = document.querySelector('#category')
+let root = document.documentElement
+let ID = 0
+let categoryIcon
+let selectCategory
+let moneyArray = [0]
 
-const saveBtn = document.querySelector('.save')
-const cancelBtn = document.querySelector('.cancel')
+const showPanel = () => {
+	addTransactionPanel.style.display = 'flex'
+}
+const closePanel = () => {
+	addTransactionPanel.style.display = 'none'
+    clearInputs()
+}
+const checkForm = () => {
+	if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
+		console.log('ok')
+	} else {
+		alert('Uzupełnij wszystkie pola!')
+	}
+}
+
+const clearInputs = () => {
+	nameInput.value = ''
+	amountInput.value = ''
+	categorySelect.selectedIndex = 0
+}
+
+addBtn.addEventListener('click', showPanel)
+cancelBtn.addEventListener('click', closePanel)
+saveBtn.addEventListener('click', checkForm)
