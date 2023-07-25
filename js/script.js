32db4fb05851dd696d7b729cc4922eb50ef4a653
Radek <rj.airoo@gmail.com>
2023-07-25 22:57:12 +0200

    added createNewTransaction function

--- js/script.js
+++ js/script.js
@@ -27,10 +27,12 @@ let moneyArray = [0]
 const showPanel = () => {
 	addTransactionPanel.style.display = 'flex'
 }
+
 const closePanel = () => {
 	addTransactionPanel.style.display = 'none'
-    clearInputs()
+	clearInputs()
 }
+
 const checkForm = () => {
 	if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
 		console.log('ok')
@@ -43,6 +45,20 @@ const clearInputs = () => {
 	nameInput.value = ''
 	amountInput.value = ''
 	categorySelect.selectedIndex = 0
+}
+
+const createNewTransaction = () => {
+	const newTransaction = document.createElement('div')
+	newTransaction.classList.add('transaction')
+	newTransaction.setAttribute('id', ID)
+
+	newTransaction.innerHTML = `
+	<p class="transaction-name">${categoryIcon} ${nameInput.value} </p>
+	<p class="transaction-amount">${amountInput.value} zł 
+	<button class="delete" onclick="deleteTransaction(${ID})"><i class="fas fa-times"></i></button> </p>
+	`
+
+	amountInput.value > 0 ? incomeArea.appendChild(newTransaction) && newTransaction.classList.add('income') : expensesArea.appendChild(newTransaction) && newTransaction.classList.add('expense')
 }
 
 addBtn.addEventListener('click', showPanel)
